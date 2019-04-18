8a1bf929503894326851f7dc41a256acee36d33b
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-18 16:23:21 +0300

    added base template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,6 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args){
         staticFileLocation("/public");
+        String layout ="templates/layout.vtl";
     }
 }
