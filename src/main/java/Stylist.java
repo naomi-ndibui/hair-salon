b9fbc4353215a8b0aac44a736603801416b344e5
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-21 12:22:16 +0300

    declared variables and setas private

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,2 +1,10 @@
 public class Stylist {
+    private String name;
+    private String image;
+    private int id;
+
+    public Stylist (String name,String image ){
+        this.name =name;
+        this.image = image;
+    }
 }
