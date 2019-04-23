cf2090a5888c72c981a5b49db44cff3b062b4656
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-23 13:21:13 +0300

    aaded listing of clients

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -50,4 +50,12 @@ public class Stylist {
             return stylist;
         }
     }
+    public List<Client> getClients(){
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where stylistid=:id";
+            return con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeAndFetch(Client.class);
+        }
+    }
 }
