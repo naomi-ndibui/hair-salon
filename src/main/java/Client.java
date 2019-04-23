bb32bc98f8420df1f08169457042b863dfcf257e
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-23 09:34:02 +0300

    In order to compare objects we retrieve from the database with our code above, we must override the .equals() method.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,7 +7,7 @@ public class Client {
     private int stylistId;
     private int id;
 
-    public  Client (String name, int stylistId){
+    public Client(String name, int stylistId) {
         this.name = name;
         this.stylistId = stylistId;
     }
@@ -23,10 +23,22 @@ public class Client {
     public int getStylistId() {
         return stylistId;
     }
+
     public static List<Client> all() {
         String sql = "SELECT id,name, stylistId FROM clients";
-        try(Connection con = DB.sql2o.open()) {
+        try (Connection con = DB.sql2o.open()) {
             return con.createQuery(sql).executeAndFetch(Client.class);
+        }
+    }
+
+    public boolean equals(Object otherClient) {
+        if (!(otherClient instanceof Client)) {
+            return false;
+        } else {
+            Client newClient = (Client) otherClient;
+            return this.getName().equals(newClient.getName()) &&
+                    this.getId() == newClient.getId() &&
+                    this.getStylistId() == newClient.getStylistId();
         }
     }
 }
