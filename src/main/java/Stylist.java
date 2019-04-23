b55204411b8d7bd56e5142087335ce095f82c7f7
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-04-23 13:14:42 +0300

    overriding equals method

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -30,5 +30,14 @@ public class Stylist {
             return con.createQuery(sql).executeAndFetch(Stylist.class);
         }
     }
-    
+    @Override
+    public boolean equals(Object otherStylist){
+        if (!(otherStylist instanceof Stylist)) {
+            return false;
+        }else{
+            Stylist newStylist =(Stylist) otherStylist;
+            return this.getName().equals(newStylist.getName()) &&
+                    this.getId() == newStylist.getId();
+        }
+    }
 }
